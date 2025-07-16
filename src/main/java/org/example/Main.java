df0dffee9fa7956d36bf758f5bc8241989e7fbdc
GowriMallidi <satyamallidi1828@gmail.com>
2025-07-16 15:27:21 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver=new ChromeDriver();
+//        driver.get("https://www.google.com");
+//        driver.manage().window().maximize();
+
+    }
+}
---
+++ src/test/java/program1.java
@@ -0,0 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program1
+{
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+
+    }
+}
