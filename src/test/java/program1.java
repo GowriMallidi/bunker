4f1102395e4eb3e6d7494918135593b03a3e4302
GowriMallidi <satyamallidi1828@gmail.com>
2025-07-21 11:34:40 +0530

    second commit

--- src/test/java/program1.java
+++ src/test/java/program1.java
@@ -2,6 +2,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class program1
 {
     public static void main(String[] args) {
@@ -11,6 +13,7 @@ public class program1
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 
     }
