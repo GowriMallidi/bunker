8fe63d6a9d5ad7be49fe07ee3590eb7e12e84072
GowriMallidi <satyamallidi1828@gmail.com>
2025-07-21 12:55:21 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Application Started!");
+     //   System.out.println("Application Started!");
         // Your Selenium or other logic can go here
     }
 }
